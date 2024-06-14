af5d72c20db1500b875fc35027e0ded9c3a607fc
pharmlovex <pharmifedayoojo@gmail.com>
2024-06-14 09:19:59 +0000

    change file location

--- Image_and_Script/image/image/test.py
+++ Image_and_Script/image/image/test.py
@@ -4,9 +4,9 @@ from imgAnalysis import process_image
 import sys
 
 
-image_mask = "mask" # os.path.dirname("mask")
-image_prop = "prop" # os.path.dirname("prop")
-image_file = "single_frame_image.tif" #os.path.basename("single_frame_image.tif")
+image_mask = "mask" 
+image_prop = "prop" 
+image_file = "single_frame_image.tif" 
 
 process_image(image_file, image_prop, image_mask)
 
